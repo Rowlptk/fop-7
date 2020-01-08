fcb1067e66b5b2fdb15a79e5bc558ee9b6887613
Rahul Pathak <rahulzha93@gmail.com>
2020-01-08 21:35:23 +0545

    js

---
+++ javaScript/app.js
@@ -0,0 +1,97 @@
+/* ---------------------------------------------------------------------------- */
+// Log to console
+console.log('Hello World');
+console.log(123);
+console.log(true);
+var greeting = 'Hello';
+console.log(greeting);
+console.log([1,2,3,4]);
+console.log({a:1, b:2});
+console.table({a:1, b:2});
+
+console.error('This is some error');
+console.clear();
+console.warn('This is a warning');
+console.time('Hello');
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+console.timeEnd('Hello');
+/*
+  multi
+  line
+  comments
+*/
+
+/* ---------------------------------------------------------------------------- */
+
+// var, let, const
+
+// var name = 'John Doe';
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
+
+// // Init var
+// var greeting;
+// console.log(greeting);
+// greeting = 'Hello';
+// console.log(greeting);
+
+// // letters, numbers, _, $
+// // Can not start with number
+
+// // Multi word vars
+// var firstName = 'John'; // Camel case
+// var first_name = 'Sara'; // Underscore
+// var FirstName = 'Tom'; // Pascal case
+// var firstname;
+
+// LET
+// let name;
+// name = 'John Doe';
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
+
+
+// CONST
+// const name = 'John';
+// console.log(name);
+// Can not reassign
+// name = 'Sara';
+// Have to assign a value
+// const greeting;
+
+
+/* ---------------------------------------------------------------------------- */
+
+// PRIMITIVE
+
+// String
+const name = 'John Doe';
+// Number
+const age = 45;
+// Boolean
+const hasKids = true;
+// Null
+const car = null;
+// Undefined
+let test;
+// Symbol
+const sym = Symbol();
+
+// REFERENCE TYPES - Objects
+// Array
+const hobbies = ['movies', 'music'];
+// Object literal
+const address = {
+  city: 'Boston',
+  state: 'MA'
+}
+const today = new Date();
+console.log(today);
+console.log(typeof today);
