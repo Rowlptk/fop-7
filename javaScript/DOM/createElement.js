82855321c0f5b0465a9aec96555214866f709834
Rahul Pathak <rahulzha93@gmail.com>
2020-01-24 18:42:52 +0545

    events

--- javaScript/DOM/createElement.js
+++ javaScript/DOM/createElement.js
@@ -12,9 +12,26 @@ li.setAttribute('title', 'New Item');
 
 // create text 
 const text = document.createTextNode('Hello World');
+// const x = document.createTextNode('Hello World ');
 
 // append
-li.appendChild(text);
+li.appendChild(text); 
+// li.appendChild(x); 
 
+// create link
+const link = document.createElement('a');
+// add classes
+link.className = 'delete-item secondary-content';
+// add icon
+link.innerHTML = '<i class="fa fa-remove"></i>';
+// link add to li
+li.appendChild(link);
+
+// append li to ul
+document.querySelector('ul.collection').appendChild(li);
+
+
+
+console.log(link);
 console.log(li);
 console.log(text);
